55624b79db9f0d1161c09bb5622ada7203a15297
GenivaldoSerra <>
2022-10-09 16:17:26 -0300

    feat: Update app.py
    Adicionando rota funcionários.

--- web_fastApi/app.py
+++ web_fastApi/app.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 
 
+funcionarios = [
+    {'id': 1, 'nome': 'Joao', 'sobrenome': 'Silva'},
+    {'id': 2, 'nome': 'Maria', 'sobrenome': 'Santos'},
+    {'id': 3, 'nome': 'Jose', 'sobrenome': 'Souza'},
+]
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/funcionarios")
+async def get_funcionarios():
+    return {'funcionarios': funcionarios}
