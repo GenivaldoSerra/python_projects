812c924b66e02b2d6cf50d530acff00621722170
Genivaldo <genivaldo.serra@live.com>
2022-10-12 20:48:01 -0300

    feat: Rota GET

---
+++ rest_api/src/hoteis.py
@@ -0,0 +1,13 @@
+from flask_restful import Resource
+
+
+hoteis_bd = [
+    {'id': 'alpha', 'nome': 'Alpha Hotel', 'estrelas': 4.3, 'diaria': 420.34, 'cidade': 'Rio de Janeiro'},
+    {'id': 'bravo', 'nome': 'Bravo Hotel', 'estrelas': 4.4, 'diaria': 380.90, 'cidade': 'Santa Catarina'},
+    {'id': 'charlie', 'nome': 'Charlie Hotel', 'estrelas': 4.1, 'diaria': 320.20, 'cidade': 'Santa Catarina'},
+    {'id': 'delta', 'nome': 'Delta Hotel', 'estrelas': 3.9, 'diaria': 240.12, 'cidade': 'Minas Gerais'},
+]
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': hoteis_bd}
