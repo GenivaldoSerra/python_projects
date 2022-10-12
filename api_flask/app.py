26886b2546e9c19d73930442485563c66279f773
Genivaldo <genivaldo.serra@live.com>
2022-10-12 14:36:52 -0300

    refactor: Correção de linter.

--- api_flask/app.py
+++ api_flask/app.py
@@ -4,9 +4,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': 'meus hoteis'}
+
 
 api.add_resource(Hoteis, '/hoteis')
 
