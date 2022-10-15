060544c1414dbf01f0f485f072e466c1b9776dd2
Genivaldo <genivaldo.serra@live.com>
2022-10-15 10:26:02 -0300

    Update app.py

--- rest_api/app.py
+++ rest_api/app.py
@@ -4,11 +4,18 @@ from flask_restful import Api
 from resources.hoteis import Hoteis, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
-    
+@app.before_first_request
+def create_data():
+    data.create_all()
+
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:id>')
 
 if __name__ == '__main__':
+    from sql_alchemy import data
+    data.init_app(app)
     app.run(debug=True)
