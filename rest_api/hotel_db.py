b1eb1aff3f8f285b6b74504dd0621984d475b2bd
Genivaldo <genivaldo.serra@live.com>
2022-10-15 09:31:23 -0300

    Update hotel_db.py
    Inserindo dados no banco.

--- rest_api/hotel_db.py
+++ rest_api/hotel_db.py
@@ -1,10 +1,14 @@
 import sqlite3
+from venv import create
 
 connection = sqlite3.connect('hotel.db')
 cursor = connection.cursor()
 
 create_table = "CREATE TABLE IF NOT EXISTS hoteis (hotel_id text PRIMARY KEY, \
                 nome text, estrelas real, diaria real, cidade text)"
+                
+create_hotel = "INSERT INTO hoteis VALUES ('alpha', 'Alpha Hotel', \
+                4.3, 420.34, 'Rio de Janeiro')"
 
 cursor.execute(create_table)
 connection.commit()
