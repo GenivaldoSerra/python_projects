7fc65d9bc269bc7ac47b15647485bf932b5db753
Genivaldo <genivaldo.serra@live.com>
2022-10-19 11:03:45 -0300

    Create main.py

---
+++ famp/secao03/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+cursos = {
+    1: {
+        "titulo": "Curso de Python",
+        "aulas": 10,
+        "horas": 20,
+    },
+    2: {
+        "titulo": "Curso de Django",
+        "aulas": 15,
+        "horas": 30,
+    },
+    3: {
+        "titulo": "Curso de FastAPI",
+        "aulas": 15,
+        "horas": 25,
+    }
+}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        debug=True,
+    )
