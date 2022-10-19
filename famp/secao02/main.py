8230575d7fccfd8e8d2552c1ba8bd2ad2b689e85
Genivaldo <genivaldo.serra@live.com>
2022-10-19 09:47:08 -0300

    Update main.py

--- famp/secao02/main.py
+++ famp/secao02/main.py
@@ -4,4 +4,14 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Testando o FastAPI!!! :)"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "main:app", 
+        host="127.0.0.1", 
+        port=8000, 
+        reload=True
+        )
